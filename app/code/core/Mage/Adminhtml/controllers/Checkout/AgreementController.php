5d0d84c4a775ccba53d3a61f17b72bad72d12291
Dharmesh Mandaliya <dharmesh.direct@gmail.com>
2013-12-27 15:50:27 +0530

    app/code/core/Mage/Adminhtml/controllers/Checkout/AgreementController.php

--- app/code/core/Mage/Adminhtml/controllers/Checkout/AgreementController.php
+++ app/code/core/Mage/Adminhtml/controllers/Checkout/AgreementController.php
@@ -47,18 +47,6 @@ class Mage_Adminhtml_Checkout_AgreementController extends Mage_Adminhtml_Control
     {
         $this->_forward('edit');
     }
-	public function dharmesh()
-    {
-        $this->_forward('edit');
-    }
-	public function tapan()
-    {
-        $this->_forward('edit');
-    }
-	public function vinod()
-    {
-        $this->_forward('edit');
-    }
     public function editAction()
     {
         $this->_title($this->__('Sales'))->_title($this->__('Terms and Conditions'));
